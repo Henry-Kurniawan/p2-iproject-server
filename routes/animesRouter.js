a27908d5a8ef33d340712edd3b32dbc689d98df8
Henry-Kurniawan <henryksutanto@gmail.com>
2021-09-22 02:28:56 +0700

    Fix error messages

--- routes/animesRouter.js
+++ routes/animesRouter.js
@@ -1,8 +1,11 @@
 const express = require("express")
 const animesRouter = express.Router()
 const ControllerAnime = require("../controllers/controllerAnime")
+const errorHandler = require("../middleware/errorHandler")
 
 animesRouter.get("/", ControllerAnime.getAnimeList)
 animesRouter.get("/:id", ControllerAnime.getAnimeDetail)
+
+animesRouter.use(errorHandler)
 
 module.exports = animesRouter
