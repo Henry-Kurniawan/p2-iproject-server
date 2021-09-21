465a96c7630c7841068289ba771ef4f374a7b7cd
Henry-Kurniawan <henryksutanto@gmail.com>
2021-09-21 18:14:49 +0700

    Add: filtering fuction for get anime list

--- controllers/controllerAnime.js
+++ controllers/controllerAnime.js
@@ -3,6 +3,47 @@ const axios = require("axios")
 class ControllerAnime {
     static async getAnimeList(req, res, next) {
         try {
+            let {title, status, genres, formats, page, per_page} = req.query
+            
+            if(!page || page < 1) {
+                page = 1
+            } else {
+                page = Number(page)
+            }
+            if(!per_page || per_page < 0) {
+                per_page = 9
+            } else {
+                per_page = Number(per_page)
+            }
+            
+            // =============== FILTERING ============ //
+            let queryParams = {
+                page,
+                per_page
+            }
+
+            // testing genres
+            genres = ['Action']
+            /////////////////
+            
+            if(title) {
+                queryParams.title = title
+            }
+
+            if(status) {
+                queryParams.status = Number(status)
+            }
+
+            if(formats) {
+                queryParams.formats = Number(formats)
+            }
+
+            if(genres.length > 0) {
+                genres = genres.join(",")
+                queryParams.genres = genres
+            }
+
+            console.log(queryParams)
             /*
 
             filter: 
@@ -32,12 +73,13 @@ class ControllerAnime {
             let result = await axios({
                     url: "https://api.aniapi.com/v1/anime",
                     method: "get",
-                    params: {
-                        page: 1,
-                        per_page: 20,
-                        title: "", 
-                        genres: "War,Action,Comedy" 
-                    },
+                    // params: {
+                    //     page: 1,
+                    //     per_page: 20,
+                    //     title: "", 
+                    //     genres: "War,Action,Comedy" 
+                    // },
+                    params: queryParams,
                     headers: {
                         'Authorization': process.env.ANIME_API_KEY,
                         'Content-Type': 'application/json',
